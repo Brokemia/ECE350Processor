989fb498d4a2f15b3a7b5805e50b11b0285b9d3e
Christopher Hall II <christopher.s.hall@duke.edu>
2023-04-23 11:14:38 -0400

    tas bin with frame counts instead of enumerations

--- Tas Translator/translator.py
+++ Tas Translator/translator.py
@@ -1,3 +1,4 @@
+import sys
 import serial, struct
 
 # Button mapping for the processor encoding
@@ -41,34 +42,59 @@ control_mapping = {
 }
 
 
-cmds = []
-with open("Tases/1A.tas") as f:
-    for line in f:
-        line = line.strip()
-        elements = line.split(",")
-        elements[0] = int(elements[0])
-        print(elements)
-        cmd = [0 for _ in range(16)]
-        for button in elements[1:]:
-            cmd[button_index[control_mapping[button]]] = 1
-        for _ in range(elements[0]):
-            cmds.append(cmd)
-        
-        print(cmd)
+def enumerate_cmds():
+    cmds = []
+    with open("Tases/1A.tas") as f:
+        for line in f:
+            line = line.strip()
+            elements = line.split(",")
+            elements[0] = int(elements[0])
+            cmd = [0 for _ in range(16)]
+            for button in elements[1:]:
+                cmd[button_index[control_mapping[button]]] = 1
+            for _ in range(elements[0]):
+                cmds.append(cmd)
+            
 
-print(cmds)
 
-# ser = serial.Serial(port='COM18', baudrate=115200)  # open serial port
-# print(ser.name)         # check which port was really used
-# for line in cmds:
-#     concated_cmd = ''.join([str(x) for x in line])
-#     print(concated_cmd, struct.pack(">H", int(concated_cmd, 2)))
-#     ser.write(struct.pack(">H", int(concated_cmd, 2)))     # write a string
-# ser.close()             # close port
+    # ser = serial.Serial(port='COM18', baudrate=115200)  # open serial port
+    # print(ser.name)         # check which port was really used
+    # for line in cmds:
+    #     concated_cmd = ''.join([str(x) for x in line])
+    #     print(concated_cmd, struct.pack(">H", int(concated_cmd, 2)))
+    #     ser.write(struct.pack(">H", int(concated_cmd, 2)))     # write a string
+    # ser.close()             # close port
 
-with open("mem_files/1a.mem", "w") as f:
-    test  = [''.join([str(x) for x in line]) for line in cmds]
-    print(test)
-    for line in cmds:
-        f.write(''.join([str(x) for x in line]))
-        f.write("\n")
+    with open("mem_files/1a.mem", "w") as f:
+        test  = [''.join([str(x) for x in line]) for line in cmds]
+        print(test)
+        for line in cmds:
+            f.write(''.join([str(x) for x in line]))
+            f.write("\n")
+
+def cmd_counts(name):
+    cmds = []
+    with open(f"Tases/{name}.tas") as f:
+        for line in f:
+            # Process Tasline
+            line = line.strip()
+            elements = line.split(",")
+            elements[0] = int(elements[0])
+
+            # Initialize binary and fill according to pressed buttons
+            cmd = [0 for _ in range(16)]
+            for button in elements[1:]:
+                cmd[button_index[control_mapping[button]]] = 1
+            
+            # Add binary and number of frames to cmds
+            cmds.append([cmd, elements[0]])
+
+    with open(f"mem_files/{name}.mem", "w") as f:
+        for line in cmds:
+            # Line is [binary, frame count]
+            f.write(''.join([str(x) for x in line[0]]))
+            f.write("\n")
+            f.write('{0:016b}'.format(line[1]))
+            f.write('\n')
+
+cmd_counts(sys.argv[1])
